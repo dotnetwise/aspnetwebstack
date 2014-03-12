99150f8851f3b24dc03e4c6cea72f594ed524e83
Laurenţiu Macovei <laureniu.macovei@dotnetwise.com>
2014-03-12 13:22:07 +0200

    merge 51

---
+++ src/System.Web.Helpers/PasswordCrypto.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Web.Helpers
+{
+    public class PasswordCrypto
+    {
+        public static PasswordCrypto Instance = new PasswordCrypto();
+        protected const int SaltSize = 128 / 8; // 128 bits
+        public virtual string HashPassword(string password)
+        {
+            return Crypto.HashPassword(password);
+        }
+        public virtual bool VerifyHashedPassword(string userName, string hashedPassword, string password)
+        {
+            return Crypto.VerifyHashedPassword(hashedPassword, password);
+        }
+        public virtual string GenerateSalt(int bytesLength = SaltSize)
+        {
+            return Crypto.GenerateSalt(bytesLength);
+        }
+        public virtual string Hash(string input, string algorithm = "sha256")
+        {
+            return Crypto.Hash(input, algorithm);
+        }
+
+        public virtual string Hash(byte[] input, string algorithm = "sha256")
+        {
+            return Crypto.Hash(input, algorithm);
+        }
+
+        public virtual string SHA1(string input)
+        {
+            return Crypto.SHA1(input);
+        }
+
+        public virtual string SHA256(string input)
+        {
+            return Crypto.SHA256(input);
+        }
+    }
+}
